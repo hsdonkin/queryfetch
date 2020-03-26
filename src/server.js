59bfc054e76c8f48a0301f29b90fae6e3e6d2795
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2020-03-26 09:27:33 -0700

    Remove test code

--- src/server.js
+++ src/server.js
@@ -5,5 +5,3 @@ const port = process.env['PORT'] || 3000;
 import { queryFetchAll } from './modules/queryFetch';
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
-
-queryFetchAll('https://www.hughsherman.com', 'li');
